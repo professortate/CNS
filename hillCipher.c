6d6efda933a60d7f6ce5ec8bbf9a431a99a9a6c4
professortate <professortate3@gmail.com>
2025-05-23 00:39:08 +0530

    Update hillCipher.c

--- hillCipher.c
+++ hillCipher.c
@@ -1,38 +1,61 @@
-#include<stdio.h> 
-#include<string.h> 
-int main() { 
-    unsigned int a[3][3] = { { 6, 24, 1 }, { 13, 16, 10 }, { 20, 17, 15 } }; 
-    unsigned int b[3][3] = { { 8, 5, 10 }, { 21, 8, 21 }, { 21, 12, 8 } }; 
-    int i, j; 
-    unsigned int c[20], d[20]; 
-    char msg[20]; 
-    int determinant = 0, t = 0; 
-    ; 
-    printf("Enter plain text\n "); 
-    scanf("%s", msg); 
-    for (i = 0; i < 3; i++) { 
-        c[i] = msg[i] - 65; 
-        printf("%d ", c[i]); 
-    } 
-    for (i = 0; i < 3; i++) { 
-        t = 0; 
-        for (j = 0; j < 3; j++) { 
-            t = t + (a[i][j] * c[j]); 
-        } 
-        d[i] = t % 26; 
-    } 
-    printf("\nEncrypted Cipher Text :"); 
-    for (i = 0; i < 3; i++) 
-        printf(" %c", d[i] + 65); 
-    for (i = 0; i < 3; i++) { 
-        t = 0; 
-        for (j = 0; j < 3; j++) { 
-            t = t + (b[i][j] * d[j]); 
-        } 
-c[i] = t % 26; 
-} 
-printf("\nDecrypted Cipher Text :"); 
-for (i = 0; i < 3; i++) 
-printf(" %c", c[i] + 65); 
-return 0; 
-} 
+#include <stdio.h>
+
+int main() {
+    // Key matrix (encryption)
+    int a[3][3] = { 
+        {6, 24, 1}, 
+        {13, 16, 10}, 
+        {20, 17, 15} 
+    };
+
+    // Inverse key matrix (decryption)
+    int b[3][3] = { 
+        {8, 5, 10}, 
+        {21, 8, 21}, 
+        {21, 12, 8} 
+    };
+
+    char msg[20];
+    int c[3], d[3];
+    int i, j, temp;
+
+    printf("Enter 3-letter uppercase message: ");
+    scanf("%s", msg);
+
+    // Convert message to numbers (A=0, B=1, ..., Z=25)
+    for (i = 0; i < 3; i++) {
+        c[i] = msg[i] - 'A';
+    }
+
+    // Encrypt: d = a * c mod 26
+    for (i = 0; i < 3; i++) {
+        temp = 0;
+        for (j = 0; j < 3; j++) {
+            temp += a[i][j] * c[j];
+        }
+        d[i] = temp % 26;
+    }
+
+    // Show encrypted text
+    printf("Encrypted Text: ");
+    for (i = 0; i < 3; i++) {
+        printf("%c", d[i] + 'A');
+    }
+
+    // Decrypt: c = b * d mod 26
+    for (i = 0; i < 3; i++) {
+        temp = 0;
+        for (j = 0; j < 3; j++) {
+            temp += b[i][j] * d[j];
+        }
+        c[i] = temp % 26;
+    }
+
+    // Show decrypted text
+    printf("\nDecrypted Text: ");
+    for (i = 0; i < 3; i++) {
+        printf("%c", c[i] + 'A');
+    }
+
+    return 0;
+}
